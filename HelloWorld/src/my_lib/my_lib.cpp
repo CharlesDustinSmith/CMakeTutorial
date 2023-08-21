015adcae92ccb52e0254ab515cfb5068fb748c0e
Charles Dustin Smith <cdsmith@mun.ca>
2023-08-21 12:59:57 -0230

    Using external libraries using Fetch Content keyword

--- HelloWorld/src/my_lib/my_lib.cpp
+++ HelloWorld/src/my_lib/my_lib.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
 
+#include <cxxopts.hpp>
+#include <fmt/format.h>
+#include <nlohmann/json.hpp>
+#include <spdlog/spdlog.h>
+
 #include "my_lib.h"
 
 void print_hello_world()
 {
-    std::cout << "Hello World.\n";
+    std::cout << "Hello World.\n" << std::endl;
+
+    std::cout << "JSON Lib Version: " << 
+        NLOHMANN_JSON_VERSION_MAJOR << "." <<
+        NLOHMANN_JSON_VERSION_MINOR << "." <<
+        NLOHMANN_JSON_VERSION_PATCH << std::endl;
+
+    std::cout << "FMT Version: " << 
+        FMT_VERSION << std::endl;
+    
+    std::cout << "CXXOPTS Version: " << 
+        CXXOPTS__VERSION_MAJOR << "." <<
+        CXXOPTS__VERSION_MINOR << "." <<
+        CXXOPTS__VERSION_PATCH << std::endl;
+
+    std::cout << "SPDLOG Version: " << 
+        SPDLOG_VER_MAJOR << "." <<
+        SPDLOG_VER_MINOR << "." <<
+        SPDLOG_VER_PATCH << std::endl;
 }
